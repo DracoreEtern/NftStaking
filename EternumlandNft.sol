f45ea2c72b594b713b6440cf3622968c67819a55
Eternumland Developer <120759045+DracoreEtern@users.noreply.github.com>
2023-01-29 20:59:51 +0100

    Update and rename EterNft.sol to EternumlandNft.sol

--- EternumlandNft.sol
+++ EternumlandNft.sol
@@ -7,16 +7,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 pragma solidity ^0.8.4;
 
-contract EterNft is ERC721Enumerable, Ownable {
+contract EternumlandNft is ERC721Enumerable, Ownable {
 
     
     using Strings for uint256;
     string public baseURI;
     string public baseExtension = ".json";
     uint256 public maxSupply = 6000;
-    uint256 public maxMintAmount = 50;
+    uint256 public maxMintAmount = 10;
     bool public paused = false;
-
+    uint8 public friendPer = 10;
+    uint256 private mintPermit;
+    
     constructor() ERC721("Eternumland NFT", "ETRNLD") {}
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -24,19 +26,23 @@ contract EterNft is ERC721Enumerable, Ownable {
 
     }
     
-    function mint(address _to, uint256 _mintAmount) public payable {
+    function mint(address _to, uint256 _mintAmount, address _code, uint256 _mintPermissions) public payable {
             uint256 supply = totalSupply();
             require(!paused);
             require(_mintAmount > 0);
             require(_mintAmount <= maxMintAmount);
             require(supply + _mintAmount <= maxSupply);
-            
+            require(_mintPermissions == mintPermit);
+        
             for (uint256 i = 1; i <= _mintAmount; i++) {
                 _safeMint(_to, supply + i);
+            
+            payable(_code).transfer(msg.value / 100 * friendPer);
             }
     }
 
-        function walletOfOwner(address _owner)
+
+       function walletOfOwner(address _owner)
         public
         view
         returns (uint256[] memory)
@@ -81,6 +87,14 @@ contract EterNft is ERC721Enumerable, Ownable {
             baseExtension = _newBaseExtension;
         }
         
+        function setmintPermit(uint256 _setmintPermit) public onlyOwner() {
+            mintPermit = _setmintPermit;
+        }
+        
+        function setFriendPer(uint8 _percentage) public onlyOwner {
+             require(_percentage <= 100);
+             friendPer = _percentage;
+        }
         function pause(bool _state) public onlyOwner() {
             paused = _state;
         }
@@ -89,4 +103,5 @@ contract EterNft is ERC721Enumerable, Ownable {
             require(payable(msg.sender).send(address(this).balance));
         }
 }
+
 
